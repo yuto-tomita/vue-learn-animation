e643465d7df37fc6e0c4b1025e01e5768d5ea06a
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2022-06-29 12:51:44 +0900

    Headerコンポーネントとエントリーポイントを追加

---
+++ src/components/ui/Header/HeaderUi.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { withDefaults, defineProps, onMounted } from 'vue'
+interface MenuProps {
+  menuPosition?: 'top' | 'left' | 'right'
+}
+// 上下にスライドするHeaderを作る
+
+const props = withDefaults(defineProps<MenuProps>(), {
+  menuPosition: 'top'
+})
+
+onMounted(() => {
+  console.log(typeof ['a', 'i', 'u'].toString())
+})
+
+</script>
+
+<template>
+  <div class="menu-wrapper">
+    {{ ['a', 'i', 'u'].toString() }}
+  </div>
+</template>
+
+<style scoped>
+.menu-wrapper{
+	width: 100%;
+  background: rgb(154, 128, 128);
+}
+</style>
